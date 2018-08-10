89a5697971f1eaebefce6b18a826e3b026a34c46
yuanxx <824264439@qq.com>
2018-08-10 15:18:22 +0800

    增加 vuex依赖

---
+++ store/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import state from '/store/state.js'
+import getters from '/store/getters.js'
+import mutations from '/store/mutations.js'
+import actions from '/store/actions.js'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+	state: state,
+	getters: getters,
+	mutations: mutations,
+	actions: actions,
+})
+
+export default store
