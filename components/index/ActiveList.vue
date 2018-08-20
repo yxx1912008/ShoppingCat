9ec57a98e27508e9125950eaaac0135b99871aec
yuanxx <824264439@qq.com>
2018-08-20 18:01:38 +0800

    增加搜索页面

---
+++ components/index/ActiveList.vue
@@ -0,0 +1,17 @@
+<template name="ActiveList">
+	<!-- 活动列表-->
+	<view class="container">
+
+	</view>
+	
+</template>
+
+<script>
+	export default {
+
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/search/index.vue
@@ -0,0 +1,35 @@
+<template>
+	<view>
+		<!-- <web-view :src="searchUrl"></web-view> -->
+		<rich-text :nodes="pageContent"></rich-text>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				searchUrl: 'http://cat.luckydeer.cn/index.php?r=index/search&s_type=1&kw=&kws=',
+				pageContent: ''
+			}
+		},
+		mounted: function () {
+			console.log('进入搜索页面');
+			uni.setNavigationBarTitle({
+				title: '搜索商品',
+			})
+
+			uni.request({
+				url: 'http://cat.luckydeer.cn/index.php?r=index/wap',
+				success: function (res) {
+					this.pageContent = res.data;
+				},
+			})
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
