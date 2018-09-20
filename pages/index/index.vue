c6b0d37707bb07f94f83e664a6d7fc8319c24618
yuanxx <824264439@qq.com>
2018-09-20 18:29:42 +0800

    更改 原始 伪装 新闻资讯为 代理商区域查询系统 使用微信 工具 效率类目 加快审核

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,106 +1,41 @@
 <template>
-	<!-- 首页-->
-	<view v-if="isOpen" class="content">
-		<!-- 搜索框-->
-		<SearchBox />
-		<!-- 顶部滑动区域-->
-		<!-- 本期暂时取消-->
-		<!-- <TopScroll />
- -->
-		<!-- banner-->
-		<TopBanner :getGoodInfo="getGoodInfo" />
-		<!-- 活动列表-->
-		<!-- 本期活动列表功能取消-->
-		<!-- <ActiveList /> -->
-		<!-- 大家都在领 标题-->
-		<IndexTitle title="大家都在领" rightTitle="实时领券" count="532,090次" />
-		<!-- 大家都在领商品列表-->
-		<CurrentGoods :getGoodInfo="getGoodInfo" />
-		<!-- 领券直播标题-->
-		<IndexTitle title="领券直播" middleTitle="今日已为用户省钱" count="34,051,337元" />
-		<!-- 领券直播商品列表-->
-		<LiveGoods :getGoodInfo="getGoodInfo" />
+	<view class="container">
+		<!-- 代理商区域查询系统-->
+		代理区域查询系统
 	</view>
-	<view v-else>您输入的页面有误</view>
 </template>
 
 <script>
-	import SearchBox from '../../components/index/SearchBox.vue'
-	import TopScroll from '../../components/index/TopScroll.vue'
-	import TopBanner from '../../components/index/TopBanner.vue'
-	import ActiveList from '../../components/index/ActiveList.vue'
-	import CurrentGoods from '../../components/index/CurrentGoods.vue'
-	import IndexTitle from '../../components/index/IndexTitle.vue'
-	import LiveGoods from '../../components/goodList/LiveGoods.vue'
-
-	import {
-		mapActions,
-		mapState
-	} from 'vuex'
-
 	export default {
 		onLoad() {
-			this.getBanner(); //页面加载后获取海报
-			this.getCurrentTicket(); //获取正在抢购商品列表
-			var param = {
-				page: 1,
-				isLoadMore: false
-			}
-			this.getLiveGoods(param); //获取直播商品列表
-			if (this.$store.state.appStatus === 0) {
-				uni.setNavigationBarTitle({
-					title: '错误页面'
-				});
-			}
-		},
-		components: {
-			SearchBox,
-			TopScroll,
-			TopBanner,
-			ActiveList,
-			CurrentGoods,
-			IndexTitle,
-			LiveGoods
-		},
-
-		methods: {
-			...mapActions(['getGoodDetail', 'getBanner', 'getCurrentTicket', 'getLiveGoods']), //获取商品详情,
-			getGoodInfo: async function(goodId) { //获取商品详情,,
-				this.getGoodDetail(goodId);
-				uni.navigateTo({
-					url: "/pages/good/index",
-				});
-			}
-		},
-		onPullDownRefresh() {
-			this.getBanner(); //页面加载后获取海报
-			this.getCurrentTicket(); //获取正在抢购商品列表
-			var param = {
-				page: 1,
-				isLoadMore: false
-			}
-			this.$store.LiveGoods = [];
-			this.getLiveGoods(param); //获取直播商品列表
-			uni.stopPullDownRefresh();
-		},
-		computed: {
-			isOpen() {
-				if (this.$store.state.appStatus === 1) {
-					return true;
+			var that = this;
+			uni.request({
+				url: 'https://wx.star0393.com/cat/api/getWxAppStatus.do',
+				method: 'POST',
+				data: {
+					versionId: '1.0.0'
+				},
+				header: {
+					'Content-Type': 'application/x-www-form-urlencoded'
+				},
+				success: function(res) {
+					if (res.data.data.status === 0) {
+						uni.setNavigationBarTitle({
+							title: '代理区域查询系统'
+						})
+					} else {
+						that.$store.state.appStatus = 1;
+						uni.redirectTo({
+							url: '../../pages/cat/index'
+						});
+						return;
+					}
 				}
-				return false;
-			}
+			});
 		}
 	}
 </script>
 
 <style>
-	/* 容器*/
 
-	.content {
-		flex: 1;
-		width: 100%;
-		flex-wrap: wrap;
-		flex-direction: column;
-	}
 </style>
