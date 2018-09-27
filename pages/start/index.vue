80ecd888be4e3af506b284e12522e29681aa55c0
yuanxx <824264439@qq.com>
2018-09-27 11:52:20 +0800

    修改小程序启动页面

--- pages/start/index.vue
+++ pages/start/index.vue
@@ -15,6 +15,36 @@
 <script>
 	export default {
 		onLoad: function() {
+			var that = this;
+			uni.request({
+				url: 'https://api.luckydeer.cn/cat/api/getWxAppStatus.do',
+				method: 'POST',
+				data: {
+					versionId: '1.0.0'
+				},
+				header: {
+					'Content-Type': 'application/x-www-form-urlencoded'
+				},
+				success: function(res) {
+					console.log(res)
+					if (res.data.data.status === 0) {
+						uni.setNavigationBarTitle({
+							title: '代理区域查询系统'
+						});
+						uni.setNavigationBarColor({
+							frontColor: '#ffffff',
+							backgroundColor: '#AA3D54'
+						});
+					} else {
+						that.$store.state.appStatus = 1;
+						uni.redirectTo({
+							url: '../../pages/cat/index'
+						});
+						return;
+					}
+				}
+			});
+
 
 		}
 	}
