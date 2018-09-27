b219c1af3173afc8792f5f605707f989c3671339
yuanxx <824264439@qq.com>
2018-09-27 10:39:25 +0800

    z增加启动页面

---
+++ pages/start/index.vue
@@ -0,0 +1,31 @@
+<template>
+	<!-- 启动页面-->
+	<view class="container">
+		<!-- 加载框-->
+		<view class="mask" @click="hide"></view>
+		<view class="popup popup-middle">
+			<view class="desc">
+				<text>正在加载...</text>
+			</view>
+		</view>
+
+	</view>
+</template>
+
+<script>
+	export default {
+		onLoad: function() {
+
+		}
+	}
+</script>
+
+<style>
+	@import "../../common/common.css";
+
+
+	.container {
+		width: 100%;
+		min-height: 100%;
+	}
+</style>
